4800bc1fbf510865034531e330c6ca7854e77834
jakub.szarubka@poccofinance.com <jakub.szarubka@poccofinance.com>
2020-03-31 10:03:47 +0200

    adding frist part of Builder in Company class

--- src/main/java/pl/futurecollars/invoice/model/Company.java
+++ src/main/java/pl/futurecollars/invoice/model/Company.java
@@ -2,76 +2,96 @@ package pl.futurecollars.invoice.model;
 
 public class Company {
 
-    public static class Builder {
-        private String name;
-        private String taxIdentificationNumber;
-        private String streetName;
-        private String postCode;
-        private String town;
-        private Company company;
-
-        public String getName() {
-            return name;
-        }
-
-        public Company.Builder withName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Company.Builder withTaxIdentificationNumber(String taxIdentificationNumber) {
-            this.taxIdentificationNumber = taxIdentificationNumber;
-            return this;
-        }
-
-        public Company.Builder withStreetName(String streetName) {
-            this.streetName = streetName;
-            return this;
-        }
-
-        public Company.Builder withPostCode(String postCode) {
-            this.postCode = postCode;
-            return this;
-        }
-
-        public Company.Builder withTown(String town) {
-            this.town = town;
-            return this;
-        }
-
-        public Company build() {
-            return company;
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getTaxIdentificationNumber() {
-        return taxIdentificationNumber;
-    }
-
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
     private String name;
     private String taxIdentificationNumber;
     private String streetName;
     private String postCode;
     private String town;
 
-    private Company() {
+    public Company() {
 
     }
+    public Company(String name, String taxIdentificationNumber, String streetName, String postCode, String town) {
+        this.name = name;
+        this.taxIdentificationNumber = taxIdentificationNumber;
+        this.streetName = streetName;
+        this.postCode = postCode;
+        this.town = town;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxIdentificationNumber() {
+        return taxIdentificationNumber;
+    }
+
+    public void setTaxIdentificationNumber(String taxIdentificationNumber) {
+        this.taxIdentificationNumber = taxIdentificationNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public static class Builder {
+    private Company company;
+
+        public Company.Builder withName(String name) {
+            company.setName(name);
+            return this;
+        }
+
+        public Company.Builder withTaxIdentificationNumber(String taxIdentificationNumber) {
+            company.setTaxIdentificationNumber(taxIdentificationNumber);
+            return this;
+        }
+
+        public Company.Builder withStreetName(String streetName) {
+            company.setStreetName(streetName);
+            return this;
+        }
+
+        public Company.Builder withPostCode(String postCode) {
+            company.setPostCode(postCode);
+            return this;
+        }
+
+        public Company.Builder withTown(String town) {
+            company.setTown(town);
+            return this;
+        }
+
+        public Company build() {
+           return company;
+        }
+    }
+
+
 
 }
