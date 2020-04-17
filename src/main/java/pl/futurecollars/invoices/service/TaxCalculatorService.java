7adbdb17e177f1d1925460361f95f937e7ea77b8
jakub.szarubka@poccofinance.com <jakub.szarubka@poccofinance.com>
2020-04-17 20:30:16 +0200

    adding tax caclc class

--- src/main/java/pl/futurecollars/invoices/service/TaxCalculatorService.java
+++ src/main/java/pl/futurecollars/invoices/service/TaxCalculatorService.java
@@ -1,46 +1,66 @@
 package pl.futurecollars.invoices.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.futurecollars.invoices.database.Database;
 import pl.futurecollars.invoices.model.Company;
 import pl.futurecollars.invoices.model.Invoice;
+import pl.futurecollars.invoices.model.InvoiceEntry;
 import pl.futurecollars.invoices.model.PostalAddress;
+import pl.futurecollars.invoices.model.Vat;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
-import static java.math.BigDecimal.*;
-
-
+@Service
 public class TaxCalculatorService {
 
-    final Company futureCollars = new Company("0001", "FutureCollars Sp. z o.o.", new PostalAddress("Koronawirusa", "1", "1", "01-001", "Warszawa"));
-    List<Invoice> invoices = new ArrayList<>();
-    Map<Integer, Invoice> invoiceMap = new HashMap<>();
-    Invoice invoice;
-    Integer integer;
-    Function<Invoice, BigDecimal> function;
-    BigDecimal sum;
+    @Autowired
+    private Database database;
+    private final Company futureCollars = new Company("0001", "FutureCollars Sp. z o.o.", new PostalAddress("Koronawirusa", "1", "1", "01-001", "Warszawa"));
+    private Invoice invoice;
+    private BigDecimal sumOfIncomeVAT;
+    private BigDecimal sumOfIncomeCosts;
+    private BigDecimal sumOfCosts;
+    private BigDecimal incomeToCosts;
 
+    public BigDecimal calculateAllEntriesValuesFromOneInvoice(Vat vat) {
+        BigDecimal temporaryValue = BigDecimal.valueOf(0.0);
+        for (InvoiceEntry entries : invoice.getEntries()) {
+            temporaryValue = entries.getNetPrice().multiply(vat.getRate());
+            temporaryValue.add(temporaryValue);
+        }
+        return temporaryValue;
+    }
 
-    public BigDecimal taxCalculator(Company company) {
-        invoices
+    public BigDecimal calculationOfIncomeVat(Company company) {
+        database.getInvoices()
+                .stream()
+                .filter(value -> (invoice.getSeller().getTaxIdentificationNumber() == company.getTaxIdentificationNumber()))
+                .forEach(value -> calculateAllEntriesValuesFromOneInvoice(Vat.VAT_23));
+        return sumOfIncomeVAT;
+    }
+
+    public BigDecimal calculationOfIncomeCosts(Company company) {
+        database.getInvoices()
                 .stream()
                 .filter(value -> (invoice.getBuyer().getTaxIdentificationNumber() == company.getTaxIdentificationNumber()))
-                .forEach(value -> sum +=  );
-        return valueOf(001);
+                .forEach(value ->calculateAllEntriesValuesFromOneInvoice(Vat.VAT_8));
+        return sumOfIncomeVAT;
+    }
+
+    public BigDecimal calculationOfCosts() {
+        incomeToCosts = sumOfIncomeVAT.add(sumOfCosts);
+        return incomeToCosts;
+    }
+
+    public void calculationOfOutcomeVat(Company company) {
+        // NotEnoughKnowledgeAboutTaxes exception
     }
 
 }
 
-
-
-
-//TODO policzyć wartości income VAT   outcome VAT     /income  costs  / Ostatnia wartość liczona jest z poprzednich income cost (income - costs)
-//TODO napisać 4 funkcji które będą obliczać wartości
-//TODO Podajemy ID swojej firmy , iterujemy po wszystkich fakturach na koniec liczymy income, outcome , income VAT, costs oraz income cost(income - costs)
+//TODO policzyć wartości income VAT   outcome VAT     /income / costs  / Ostatnia wartość liczona jest z poprzednich income cost (income - costs)
+//TODO napisać 4 funkcji które będą obliczać wartości. Podajemy ID swojej firmy , iterujemy po wszystkich fakturach na koniec liczymy income, outcome , income VAT, costs oraz income cost(income - costs)
 //TODO zahardkodować swoją firmę
 //TODO policzone wartości zwracamy w Json
 //TODO Stworzyć generyczną funkcję, która przyjmuje funkcję którą wsadzamy w jednego if
